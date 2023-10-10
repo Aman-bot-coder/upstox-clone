a0227858294cc0b1ed362f58cc4094751049fa9e
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-10-11 01:48:03 +0530

    inital HTML structure

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='flex'>
+            <div id="sideMenu" className='w25'>
+                <div id="indexesLiveDisplay" className='flex'>
+                    <div id="niftyLive" className='w50'>
+
+                    </div>
+
+                    <div id="sensexLive" className='w50'>
+
+                    </div>
+                </div>
+
+                <div id="watchLists">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
